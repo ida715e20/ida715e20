b0810bd27d1698c256de3c94a4bb32caacf97ceb
Magnus Hein <you@example.com>
2020-12-02 23:17:09 +0100

    ny

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,9 @@
 let children=localStorage.getItem("childrencount");
 let students=localStorage.getItem("studentscount");
 let adults=localStorage.getItem("adultscount");
-let tickets=localStorage.getItem("ticketscount")
+let tickets=localStorage.getItem("ticketscount");
+let theme=localStorage.getItem("themetour");
+let cost=localStorage.getItem("tourcost");
 
 if(children==null) children=0;
 if(students==null) students=0;
@@ -11,16 +13,21 @@ document.getElementById("childrencount_id").innerHTML = children;
 document.getElementById("studentscount_id").innerHTML = students;
 document.getElementById("adultscount_id").innerHTML = adults;
 document.getElementById("ticketscount_id").innerHTML = tickets;
+document.getElementById("theme_id").innerHTML = theme;
+document.getElementById("cost_id").innerHTML = cost;
 
 function getdata(){
     let child=document.getElementById('childticket').value;
     let student=document.getElementById('studentticket').value;
     let adult=document.getElementById('adultticket').value;
-    let count=child+student+adult;
+    let count = parseInt(child) + parseInt(student) + parseInt(adult);
+    let theme1=document.getElementById('theme1tour').value;
+    let cost = 
     
     localStorage.setItem("childrencount", child);
     localStorage.setItem("studentscount", student);
     localStorage.setItem("adultscount", adult);
-    localStorage.setItem("ticketcount", count);
- 
+    localStorage.setItem("ticketscount", count);
+    localStorage.setItem("themetour", theme1);
+    localStorage.setItem("tourcost");
 }
