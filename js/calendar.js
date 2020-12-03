8d4c2873eca96fa35255451dd57dbfb73e80cf10
Maria <mba16@student.aau.dk>
2020-12-03 11:43:48 +0100

    create event

--- js/calendar.js
+++ js/calendar.js
@@ -1,13 +1,49 @@
 
+initCalender();  
+getTourData();
+
+var calendar; 
+
+function initCalender() {
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar'); 
+    calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth', 
+      firstDay: 1,
+      events: []
    
-   document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-        
-        // initialView: 'dayGridMonth'
-        firstDay: 1
-        
-        });
-        
-        calendar.render();
     });
+  
+    calendar.render();
+  });
+
+
+}
+
+
+function getTourData(){
+
+  var tourTitle = document.getElementById('fticketTheme').value;
+  var tourDate =  document.getElementById('fdate').value;
+  var date = new Date(tourDate + 'T00:00:00'); 
+
+  calendar.addEvent({
+    title: tourTitle,
+    start: date,
+   
+  });
+}
+
+
+
+
+  
+  // var obj = {
+  //   title: tourTitle,
+  //   start: date
+
+  // }; 
+
+  // tours.push(obj); 
+
+ 
