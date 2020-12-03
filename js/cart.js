21debebeb7dd48a5925e8e5d3c6a371923ff5e69
Magnus Hein <you@example.com>
2020-12-03 23:38:12 +0100

    ny

--- js/cart.js
+++ js/cart.js
@@ -13,24 +13,41 @@ if (s) s.innerHTML = myJSON.students;
 let a=document.getElementById("adultscount_id");
 if (a) a.innerHTML = myJSON.adults;
 
+let tc=document.getElementById("ticketscount_id");
+if (tc) tc.innerHTML = parseInt(myJSON.children) + parseInt(myJSON.students) + parseInt(myJSON.adults);
 
 
+let th=document.getElementById("theme_id");
+if (th) th.innerHTML = myJSON.theme;
+
+let childticket_price = 40
+let studentticket_price = 60
+let adultticket_price = 80
+
+let t=document.getElementById("totalcost_id");
+if (t) t.innerHTML = parseInt(myJSON.children * childticket_price) + parseInt(myJSON.students * studentticket_price) + parseInt(myJSON.adults * adultticket_price);
+/*
 // fix den her - laves som de andre //
 document.getElementById("ticketscount_id").innerHTML = parseInt(myJSON.children) + parseInt(myJSON.students) + parseInt(myJSON.adults);
 
 // fix den her - laves som de andre //
 document.getElementById("theme_id").innerHTML = myJSON.theme;
+*/
 
-
-
-
+/*
+<script type="number1">
+document.getElementById("childticket").value = 40;
+</script>
+*/
 
 
 function getdata(){
+    
     let child=document.getElementById('childticket').value;
     let student=document.getElementById('studentticket').value;
     let adult=document.getElementById('adultticket').value;
     let theme=document.getElementById('themetour').innerHTML;
+    
  
     // Storing data:
     cart = {children: child, students: student, adults: adult, theme: theme};
