e86a76e175a5e103e49fecd26f40cdb411adceb4
Magnus Hein <Magnus@Magnuss-MacBook-Air.local>
2020-12-05 18:37:08 +0100

    ny

--- js/calendar.js
+++ js/calendar.js
@@ -9,21 +9,29 @@ function initCalender()
   {
     var calendarEl = document.getElementById('calendar'); 
     calendar = new FullCalendar.Calendar(calendarEl, 
-    {
- 
-      
+    { 
       firstDay: 1,
       events: []
-    
     });
     
     calendar.render();
-
-    // retrieving data //
-    tourtext = localStorage.getItem("tourJSON");
-    tourJSON = JSON.parse(tourtext);
     
 
+    // retrieving data //
+    for (let i = 1; i < localStorage.length + 1; i++)
+    {
+      tourtext = localStorage.getItem("tourJSON" + i.toString()); //håber der står tourjSON1, tourJSON2 osv
+      tourJSON = JSON.parse(tourtext);
+
+      calendar.addEvent({  
+        
+        title: tourJSON.title,
+        date: tourJSON.date,
+        start: tourJSON.start,
+        participants: tourJSON.participants,
+
+      });
+    }
   });
 }
 
@@ -34,20 +42,27 @@ function getTourData(){
   var date = new Date(tourDate + 'T00:00:00');
   var maxP = document.getElementById('fmaxParticipants').value; 
 
-
   // Storing data:
   tour = {title: tourTitle, date: tourDate, start: date, participants: maxP};
   tourJSON = JSON.stringify(tour);
-  localStorage.setItem("tourJSON", tourJSON);
+
+  let numberevents = localStorage.length + 1;
+  
+  localStorage.setItem("tourJSON" + numberevents.toString(), tourJSON);
+        
+
 
   calendar.addEvent({  
     
-    title: tourJSON.title,
-    date: tourJSON.date,
-    start: tourJSON.start,
-    participants: tourJSON.participants,
+    title: tourTitle,
+    date: tourDate,
+    start: date,
+    participants: maxP,
 
   });
+/*
+  console.log(calendar.getEventById("noget"))
+*/
 }
 
 
