12973493f1b166783d7c67f330200a20fd5c7f96
Maria <mba16@student.aau.dk>
2020-12-06 14:21:00 +0100

    Modal vindue

--- js/calendar.js
+++ js/calendar.js
@@ -1,18 +1,24 @@
 
-
-var calendar; 
+var calendar, tours = [ {id: 1, title: "lort", start: '2020-12-12'}, {id: 2, title: "haha", start: '2020-12-21'}]
 
 initCalender();
 
+
 function initCalender() {
-  console.log("hej");
   document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar'); 
     calendar = new FullCalendar.Calendar(calendarEl, {
  
       firstDay: 1,
-      events: []
+      events: tours,      
    
+      eventClick: function(event){
+        $('#eventModal').modal('show'); 
+        $('.modal-title').html(event.event.title); 
+        $('.modal-body').html(event.event.start); 
+       
+      }
+
     });
     
     calendar.render();
@@ -20,32 +26,17 @@ function initCalender() {
     // retrieving data //
     tourtext = localStorage.getItem("tourJSON");
     tourJSON = JSON.parse(tourtext);
-    
 
-    calendar.addEvent({  
+    console.log(tourJSON);
     
-      title: tourJSON.title,
-      date: tourJSON.date,
-      start: tourJSON.start,
-      participants: tourJSON.participants,
-
-    });
 
   });
 }
 
+function modalTest (){
+
+
+}
 
 
 
-function getTourData(){
-
-  var tourTitle = document.getElementById('fticketTheme').value;
-  var tourDate =  document.getElementById('fdate').value;
-  var date = new Date(tourDate + 'T00:00:00');
-  var maxP = document.getElementById('fmaxParticipants').value; 
-
-
-  // Storing data:
-  tour = {title: tourTitle, date: tourDate, start: date, participants: maxP};
-  tourJSON = JSON.stringify(tour);
-  localStorage.setItem("tourJSON", tourJSON);
