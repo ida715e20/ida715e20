befbc6d9f83fce291d310edfc22eac3c99f323a0
Maria <mba16@student.aau.dk>
2020-12-06 12:04:29 +0100

    s

--- js/calendar.js
+++ js/calendar.js
@@ -1,33 +1,51 @@
-$(function() {
 
-  $('#calendar').fullCalendar({
-    defaultView: 'month',
 
-    header: {
-      center: 'addEventButton'
-    },
+var calendar; 
 
-    customButtons: {
-      addEventButton: {
-        text: 'add event...',
-        click: function() {
-          var dateStr = prompt('Enter a date in YYYY-MM-DD format');
-          var date = moment(dateStr);
+initCalender();
 
-          if (date.isValid()) {
-            $('#calendar').fullCalendar('renderEvent', {
-              title: 'dynamic event',
-              start: date,
-              allDay: true
-            });
-            alert('Great. Now, update your database...');
-          } else {
-            alert('Invalid date.');
-          }
-        }
-      }
-    }
+function initCalender() {
+  console.log("hej");
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar'); 
+    calendar = new FullCalendar.Calendar(calendarEl, {
+ 
+      firstDay: 1,
+      events: []
+   
+    });
+    
+    calendar.render();
+
+    // retrieving data //
+    tourtext = localStorage.getItem("tourJSON");
+    tourJSON = JSON.parse(tourtext);
+    
+
+    calendar.addEvent({  
+    
+      title: tourJSON.title,
+      date: tourJSON.date,
+      start: tourJSON.start,
+      participants: tourJSON.participants,
+
+    });
+
   });
+}
 
-});
 
+
+
+function getTourData(){
+
+  var tourTitle = document.getElementById('fticketTheme').value;
+  var tourDate =  document.getElementById('fdate').value;
+  var date = new Date(tourDate + 'T00:00:00');
+  var maxP = document.getElementById('fmaxParticipants').value; 
+
+
+  // Storing data:
+  tour = {title: tourTitle, date: tourDate, start: date, participants: maxP};
+  tourJSON = JSON.stringify(tour);
+  localStorage.setItem("tourJSON", tourJSON);
