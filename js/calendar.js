f450188b531c28775f98fa11ef29d9d09e30c951
Maria <mba16@student.aau.dk>
2020-12-06 10:56:29 +0100

    m

--- js/calendar.js
+++ js/calendar.js
@@ -1,49 +1,60 @@
 
-initCalender();  
-getTourData();
 
-var calendar; 
+
+var calendar, tours=[{
+
+  "allDay": "true",
+  "title": "Test event",
+  "id": "821",
+  "start": "2020-12-06"
+}]; 
+
+initCalender();
 
 function initCalender() {
+  console.log("hej");
   document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar'); 
     calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth', 
+ 
       firstDay: 1,
-      events: []
+      events: tours,
    
     });
-  
+
+ 
+  }
     calendar.render();
-  });
 
-
+   
 }
+
+  
+  
+
+
 
 
 function getTourData(){
 
   var tourTitle = document.getElementById('fticketTheme').value;
   var tourDate =  document.getElementById('fdate').value;
-  var date = new Date(tourDate + 'T00:00:00'); 
+  var date = new Date(tourDate + 'T00:00:00');
+  var maxP = document.getElementById('fmaxParticipants').value; 
+ 
+  // var t = {title: tourTitle, date: tourDate, start: date, participants: maxP};
+  // tours.push(t); 
 
-  calendar.addEvent({
+    var eventObject = {
     title: tourTitle,
-    start: date,
-   
-  });
+    start: tourDate,
+ 
+    };
+
+
 }
 
 
 
-
-  
-  // var obj = {
-  //   title: tourTitle,
-  //   start: date
-
-  // }; 
-
-  // tours.push(obj); 
 
  
