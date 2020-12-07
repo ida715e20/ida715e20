6fb69bf5a27cc5b866df20d3e41610ea995772e8
Maria <mba16@student.aau.dk>
2020-12-07 09:26:26 +0100

    Modal update: Remove added

--- js/calendar.js
+++ js/calendar.js
@@ -1,49 +1,110 @@
 
-var calendar, tours = [{
-
-  title: "Test1", 
-  start: '2020-12-12', 
-  description: "Hej med dig .. blablabla"
- },
-
-  {
-
-    title: "Test 2", 
-    start: '2020-12-21',
-    description: "Blablalbalba"
-
-  }]
+var calendar, choseneventid;
 
 initCalender();
 
-
-function initCalender() {
-  document.addEventListener('DOMContentLoaded', function() {
+function initCalender() 
+{
+  document.addEventListener('DOMContentLoaded', function() 
+  {
     var calendarEl = document.getElementById('calendar'); 
-    calendar = new FullCalendar.Calendar(calendarEl, {
- 
-      firstDay: 1,
-      events: tours,      
-   
-      eventClick: function(event,){
+    calendar = new FullCalendar.Calendar(calendarEl, 
+    { 
       
-          $('#eventModal').modal('show'); 
-          $('.modal-title').html(event.event.title); 
-          $('.modal-body').html(event.event.start); 
 
-      }
+      firstDay: 1,  
+      events: [],
 
-    });
+
+      eventClick: function(event,){
+
+        $('#eventTitle').html(event.event.title); 
+        $('#eventInfo').html(event.event.extendedProps.description);
+        $('#eventTickets').html(event.event.extendedProps.numberOfParticipants);
+        $('#eventPlace').html(event.event.extendedProps.place);
+        $('#eventPlace').html(event.event.id);
+
+        choseneventid = event.event.id; 
+
+        $('#eventModal').modal('show'); 
+
+
+        
+    }
     
+          
+    });
+    /*
+    calendar.on('eventClick', function (info) {
+    chosenevent = info.event.id;
+    }); 
+    */
     calendar.render();
+    
 
     // retrieving data //
-    tourtext = localStorage.getItem("tourJSON");
-    tourJSON = JSON.parse(tourtext);
+    for (let i = 1; i < localStorage.length + 1; i++)
+    {
+      tourtext = localStorage.getItem("tourJSON" + i.toString()); //håber der står tourjSON1, tourJSON2 osv
+      tourJSON = JSON.parse(tourtext);
 
-    console.log(tourJSON);
-    
-
+      calendar.addEvent({  
+        
+        id : tourJSON.id,
+        title: tourJSON.title,
+        date: tourJSON.date,
+        start: tourJSON.start,
+        participants: tourJSON.participants,
+        duration: tourJSON.duration,
+        place: tourJSON.place
+        
+      });
+    }
   });
 }
 
+function getTourData(){
+
+  var tourTitle = document.getElementById('fticketTheme').value;
+  var tourDate =  document.getElementById('fdate').value;
+  var time = new Date(tourDate + 'T00:00:00');
+  var maxP = document.getElementById('fmaxParticipants').value;
+  var tourDuration = document.getElementById('fduration').value;
+  var tourPlace = document.getElementById('fplace').value;
+  var uniqueID = getRandomInt(5000);
+
+
+
+  // Storing data:
+  tour = { id: uniqueID, title: tourTitle, date: tourDate, start: time, participants: maxP, duration: tourDuration, place: tourPlace};
+  tourJSON = JSON.stringify(tour);
+
+  let numberevents = localStorage.length + 1;
+  
+  localStorage.setItem("tourJSON" + numberevents.toString(), tourJSON);
+        
+
+
+  calendar.addEvent({  
+    
+    id: uniqueID,
+    title: tourTitle,
+    date: tourDate,
+    start: time,
+    participants: maxP,
+    duration: tourDuration,
+    place: tourPlace,
+  });
+/*
+  console.log(calendar.getEventById("noget"))
+*/
+}
+
+function removeEvent()
+{
+  calendar.getEventById(choseneventid).remove();
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
